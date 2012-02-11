7541c841d31f8704366507831015810bcf0841e0
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-02-11 16:41:53 +0100

    fix bugs and restructured code

--- userModule.rb
+++ userModule.rb
@@ -1,10 +1,15 @@
 require_relative 'Parser'
 
 class UserModule
-  parser = Parser.new
-  puts "Which days menu do you want? Press "
-  puts " 1 for Monday \n 2 for Tuesday \n 3 for Wednesday \n 4 for Thursday \n 5 for Friday"
-  day = gets
-  puts parser.get_meny[day.to_i]
+  def get_offers
+    puts "Which days menu do you want? Press "
+    puts " 1 for Monday \n 2 for Tuesday \n 3 for Wednesday \n 4 for Thursday \n 5 for Friday"
+    day = gets
+    parser = Parser.new
+    puts parser.get_meny[day.to_i - 1]
+  end
 end
+
+user = UserModule.new
+user.get_offers
 
